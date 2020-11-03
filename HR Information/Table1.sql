9cb1425e747d1b96d8b9c1ff6977a328add5b957
gahenri <73147502+gahenri@users.noreply.github.com>
2020-11-02 21:30:15 -0500

    Created HR Information Project

---
+++ HR Information/Table1.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Employee]
+(
+	[Key] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Employee Number] INT NOT NULL, 
+    [First Name] VARCHAR(20) NOT NULL, 
+    [Last Name] VARCHAR(20) NOT NULL, 
+    [SSN] VARCHAR(11) NULL, 
+    [Start Date] DATE NULL, 
+    [Start Date] DATE NULL, 
+    [Termination Date] DATE NULL, 
+    [Gender] VARCHAR(2) NULL, 
+    [Department] NCHAR(20) NULL
+)
