e86108215f0944f7c1d64e6bfe0d92622bcb9f21
gahenri <73147502+gahenri@users.noreply.github.com>
2020-11-02 20:45:55 -0500

    removed files

--- Database1/dbo/Tables/Person Table.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Person Table]
-(
-	[Person Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [First Name] NVARCHAR(50) NULL, 
-    [Last Name] NVARCHAR(50) NULL, 
-    [SSN] NUMERIC NULL
-)
--- Learn_SQL_Mike_Dene/dbo/Tables/Student.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [dbo].[Dane_Majors]
-(
-	[student_ID] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [name] VARCHAR(50) NOT NULL, 
-    [major] VARCHAR(50) NULL
-)
