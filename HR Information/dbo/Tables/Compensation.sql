7a3d3ed03a2823dc6fb5bb423720e848420713e4
gahenri <73147502+gahenri@users.noreply.github.com>
2020-11-03 21:42:15 -0500

    Published HR Information Database

---
+++ HR Information/dbo/Tables/Compensation.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Compensation]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Employee ID] INT NULL, 
+    [Paydata Employee #] INT NOT NULL, 
+    [New Payrate] NUMERIC(4, 2) NOT NULL, 
+    [New Payrate Date] DATE NOT NULL, 
+    CONSTRAINT [FK_Compensation_Employee] FOREIGN KEY ([Employee ID]) REFERENCES [Employee]([Employee ID])
+)
---
+++ HR Information/dbo/Tables/Employee.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Employee]
+(
+    [Employee ID] INT NOT NULL, 
+    [First Name] NVARCHAR(20) NULL, 
+    [Last Name] NVARCHAR(30) NOT NULL, 
+    [SSN] NCHAR(11) NULL, 
+    [Date of Birth] DATE NULL, 
+    [Date of Hire] DATE NULL, 
+    [Date of Termination] DATE NULL, 
+    [Gender Identity] NVARCHAR(10) NULL, 
+    CONSTRAINT [PK_Employee] PRIMARY KEY ([Employee ID])
+)
